06ffffec5a26b46bd4cfb04f4f113b7354d4e82d
lholznagel <contact@lholznagel.info>
2019-05-28 23:13:57 +0200

    Removed feature flags

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,23 +1,33 @@
 mod aws_cli;
 
-use aws_codebuild_status_terminal::TerminalOutput;
-#[cfg(feature = "web")]
-use aws_codebuild_status_web::WebOutput;
 use aws_codebuild_status_derive::CodebuildOutput;
-use clap::{App, Arg};
+use aws_codebuild_status_terminal::TerminalOutput;
+use aws_codebuild_status_web::WebOutput;
+use clap::{App, Arg, crate_authors, crate_description, crate_version};
 use std::collections::HashMap;
 
-fn main() { 
+fn main() {
     let matches = App::new("AWS Codebuild status")
-        .version("")
-        .author("Lars Holznagel <contact@lholznagel.info>")
-        .arg(Arg::with_name("branch")
-            .short("b")
-            .long("branch")
-            .value_name("BRANCH")
-            .help("Only shows builds from that branch")
-            .default_value("all")
-            .takes_value(true))
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("branch")
+                .short("b")
+                .long("branch")
+                .value_name("BRANCH")
+                .help("Only shows builds from that branch")
+                .default_value("all")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("web")
+                .short("w")
+                .long("web")
+                .value_name("WEB")
+                .help("Generates a static web page with additional information")
+                .takes_value(false),
+        )
         .get_matches();
 
     let branch = matches.value_of("branch").unwrap();
@@ -38,9 +48,9 @@ fn main() {
         map.insert(info.name.clone(), build_information);
     }
 
-    #[cfg(feature = "default")]
     TerminalOutput::print(map.clone());
 
-    #[cfg(feature = "web")]
-    WebOutput::print(map.clone());
+    if matches.is_present("web") {
+        WebOutput::print(map.clone());
+    }
 }
